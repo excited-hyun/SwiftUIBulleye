f97434da93bc9790dcb6e84008d581fb20fc4c6f
excited-hyun <nahyun1127@naver.com>
2021-05-09 13:58:49 +0900

    Baseline

--- SwiftUIBulleyeV1/ContentView.swift
+++ SwiftUIBulleyeV1/ContentView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible: Bool = false //state 변수 선언
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        VStack{
+            Text("Welcome to my first App!")
+                .fontWeight(.semibold)
+                .padding().foregroundColor(.green)
+            
+            
+            Button(action: {
+                print("Button Pressed!")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me!")
+                
+            }
+            .alert(isPresented: self.$alertIsVisible) {
+                Alert(title: Text("Hello There!"),
+                      message: Text("This is my first pop-up."),
+                      dismissButton: .default(Text("Awesome!")))
+            }//end of .alert
+        }//end of VStack
+    }//end of body
 }
 
 struct ContentView_Previews: PreviewProvider {
